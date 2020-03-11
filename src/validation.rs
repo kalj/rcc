2ddec642ae79568cb907ddac0e865249d73a1871
Karl Ljungkvist <k.ljungkvist@gmail.com>
2020-03-11 18:10:35 +0100

    simplify validation

--- src/validation.rs
+++ src/validation.rs
@@ -21,34 +21,38 @@ struct Func {
 
 struct Validator {
     errors: Vec<ValidationError>,
+    function_map: HashMap<String, Func>,
 }
 
 impl Validator {
     fn new() -> Validator {
-        Validator { errors: Vec::new() }
+        Validator {
+            errors: Vec::new(),
+            function_map: HashMap::new(),
+        }
     }
 
-    fn validate_expression(&mut self, expr: &Expression, fmap: &HashMap<String, Func>) {
+    fn validate_expression(&mut self, expr: &Expression) {
         match expr {
-            Expression::Assign(_, _, expr, _) => self.validate_expression(expr, fmap),
+            Expression::Assign(_, _, expr, _) => self.validate_expression(expr),
             Expression::BinaryOp(_, e1, e2) => {
-                self.validate_expression(e1, fmap);
-                self.validate_expression(e2, fmap);
+                self.validate_expression(e1);
+                self.validate_expression(e2);
             }
-            Expression::UnaryOp(_, expr) => self.validate_expression(expr, fmap),
+            Expression::UnaryOp(_, expr) => self.validate_expression(expr),
             Expression::Conditional(e1, e2, e3) => {
-                self.validate_expression(e1, fmap);
-                self.validate_expression(e2, fmap);
-                self.validate_expression(e3, fmap);
+                self.validate_expression(e1);
+                self.validate_expression(e2);
+                self.validate_expression(e3);
             }
             Expression::FunctionCall(name, args, ctx) => {
                 for arg in args {
-                    self.validate_expression(arg, fmap);
+                    self.validate_expression(arg);
                 }
 
-                if !fmap.contains_key(name) {
+                if !self.function_map.contains_key(name) {
                     self.errors.push(ValidationError::new(format!("Missing declaration of function '{}'", name), ctx));
-                } else if fmap[name].nparam != args.len() {
+                } else if self.function_map[name].nparam != args.len() {
                     self.errors.push(ValidationError::new(format!("Too many arguments to function '{}'", name), ctx));
                 }
             }
@@ -56,62 +60,62 @@ impl Validator {
         }
     }
 
-    fn validate_statement(&mut self, stmt: &Statement, fmap: &HashMap<String, Func>) {
+    fn validate_statement(&mut self, stmt: &Statement) {
         match stmt {
-            Statement::Return(expr) => self.validate_expression(expr, fmap),
-            Statement::Expr(expr) => self.validate_expression(expr, fmap),
+            Statement::Return(expr) => self.validate_expression(expr),
+            Statement::Expr(expr) => self.validate_expression(expr),
             Statement::If(cond, ifbody, elsebody) => {
-                self.validate_expression(cond, fmap);
-                self.validate_statement(ifbody, fmap);
+                self.validate_expression(cond);
+                self.validate_statement(ifbody);
                 if let Some(eb) = elsebody {
-                    self.validate_statement(eb, fmap);
+                    self.validate_statement(eb);
                 }
             }
-            Statement::Compound(bkitems) => self.validate_block_items(bkitems, fmap),
+            Statement::Compound(bkitems) => self.validate_block_items(bkitems),
             Statement::For(initexpr, cond, postexpr, body) => {
                 if let Some(ie) = initexpr {
-                    self.validate_expression(ie, fmap);
+                    self.validate_expression(ie);
                 }
-                self.validate_expression(cond, fmap);
+                self.validate_expression(cond);
                 if let Some(pe) = postexpr {
-                    self.validate_expression(pe, fmap);
+                    self.validate_expression(pe);
                 }
-                self.validate_statement(body, fmap);
+                self.validate_statement(body);
             }
             Statement::ForDecl(decl, cond, postexpr, body) => {
-                self.validate_declaration(decl, fmap);
-                self.validate_expression(cond, fmap);
+                self.validate_declaration(decl);
+                self.validate_expression(cond);
                 if let Some(pe) = postexpr {
-                    self.validate_expression(pe, fmap);
+                    self.validate_expression(pe);
                 }
-                self.validate_statement(body, fmap);
+                self.validate_statement(body);
             }
             Statement::While(cond, body) => {
-                self.validate_expression(cond, fmap);
-                self.validate_statement(body, fmap);
+                self.validate_expression(cond);
+                self.validate_statement(body);
             }
             Statement::DoWhile(body, cond) => {
-                self.validate_statement(body, fmap);
-                self.validate_expression(cond, fmap);
+                self.validate_statement(body);
+                self.validate_expression(cond);
             }
             _ => {}
         }
     }
 
-    fn validate_declaration(&mut self, decl: &Declaration, fmap: &HashMap<String, Func>) {
+    fn validate_declaration(&mut self, decl: &Declaration) {
         if let Some(expr) = &decl.init {
-            self.validate_expression(&expr, fmap);
+            self.validate_expression(&expr);
         }
     }
 
-    fn validate_block_items(&mut self, bkitems: &[BlockItem], fmap: &HashMap<String, Func>) {
+    fn validate_block_items(&mut self, bkitems: &[BlockItem]) {
         for bkitem in bkitems {
             match bkitem {
                 BlockItem::Stmt(stmt) => {
-                    self.validate_statement(&stmt, fmap);
+                    self.validate_statement(&stmt);
                 }
                 BlockItem::Decl(decl) => {
-                    self.validate_declaration(&decl, fmap);
+                    self.validate_declaration(&decl);
                 }
             }
         }
@@ -121,7 +125,6 @@ impl Validator {
         &mut self,
         id: &str,
         parameters: &[FunctionParameter],
-        fmap: &HashMap<String, Func>,
         ctx: &AstContext,
     ) {
         let nparam = parameters.len();
@@ -137,8 +140,8 @@ impl Validator {
             }
         }
 
-        if fmap.contains_key(id) {
-            let f = &fmap[id];
+        if self.function_map.contains_key(id) {
+            let f = &self.function_map[id];
 
             // check for different number of parameters
             if f.nparam != nparam {
@@ -153,39 +156,42 @@ impl Validator {
         }
     }
 
-    fn validate(&mut self, prog: &Program) {
-        let mut function_map = HashMap::<String, Func>::new();
+    fn validate_function(&mut self, func: &Function) {
+        match func {
+            Function::Declaration(id, parameters, ctx) => {
+                self.validate_function_declaration(id, parameters, ctx);
+
+                if !self.function_map.contains_key(id) {
+                    self.function_map.insert(id.to_string(), Func { nparam: parameters.len(), defined: false });
+                }
+            }
+            Function::Definition(id, parameters, body, ctx) => {
+                self.validate_function_declaration(id, parameters, ctx);
+
+                if self.function_map.contains_key(id) {
+                    let f = &self.function_map[id];
+
+                    // check if already defined
+                    if f.defined {
+                        self.errors.push(ValidationError::new(format!("Redefinition of '{}'", id), ctx));
+                    } else {
+                        self.function_map.get_mut(id).unwrap().defined = true;
+                    }
+                } else {
+                    self.function_map.insert(id.to_string(), Func { nparam: parameters.len(), defined: true });
+                }
+
+                self.validate_block_items(body);
+            }
+        }
+    }
+
+    fn validate_program(&mut self, prog: &Program) {
 
         let Program::Prog(funcs) = prog;
 
         for func in funcs {
-            match func {
-                Function::Declaration(id, parameters, ctx) => {
-                    self.validate_function_declaration(id, parameters, &function_map, ctx);
-
-                    if !function_map.contains_key(id) {
-                        function_map.insert(id.to_string(), Func { nparam: parameters.len(), defined: false });
-                    }
-                }
-                Function::Definition(id, parameters, body, ctx) => {
-                    self.validate_function_declaration(id, parameters, &function_map, ctx);
-
-                    if function_map.contains_key(id) {
-                        let f = &function_map[id];
-
-                        // check if already defined
-                        if f.defined {
-                            self.errors.push(ValidationError::new(format!("Redefinition of {}", id), ctx));
-                        } else {
-                            function_map.get_mut(id).unwrap().defined = true;
-                        }
-                    } else {
-                        function_map.insert(id.to_string(), Func { nparam: parameters.len(), defined: true });
-                    }
-
-                    self.validate_block_items(body, &function_map);
-                }
-            }
+            self.validate_function(func);
         }
     }
 }
@@ -193,7 +199,7 @@ impl Validator {
 pub fn validate(prog: &Program) -> Vec<ValidationError> {
     let mut val = Validator::new();
 
-    val.validate(prog);
+    val.validate_program(prog);
 
     val.errors
 }
